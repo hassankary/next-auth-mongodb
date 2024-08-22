0fa2cca9e6d2be917d025c97e35b6e70d42a99aa
Hassan Kaeru <hassanaskary29@gmail.com>
2024-08-22 20:39:03 +0700

    add login, register, & dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/LoginForm";
+import { RegisterForm } from "@/components/RegisterForm";
+import Image from "next/image";
+
+export default function Dashboard() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        Dashboard
+    </div>
+  );
+}
