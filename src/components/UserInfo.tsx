ed5d11b8d01d09e4d49712c4e397d803a0e8fc20
Hassan Kaeru <hassanaskary29@gmail.com>
2024-08-24 01:28:47 +0700

    update: UserInfo

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,20 +1,26 @@
 "use client";
-import React from "react";
+import { signOut, useSession } from "next-auth/react";
+import React, { useEffect } from "react";
 
 export const UserInfo: React.FC = () => {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    console.log("session", session)
+  }, [])
+
   return (
     <div className="border-y-2 border-green-500 font-bold px-10 py-5 rounded-2xl shadow-xl space-y-5">
-        <h1 className="font-bold text-center"> Data User</h1>
+      <h1 className="font-bold text-center"> Data User</h1>
       <div className="flex flex-col gap-1">
-        <div>
-          Hassan Askary
-        </div>
-        <div>
-          hassanaskary29@gmail.com
-        </div>
+        <div>{session?.user.name}</div>
+        <div>{session?.user?.email}</div>
       </div>
       <div className="flex justify-center">
-        <button className="flex w-fit px-3 py-1 bg-red-500 text-white rounded-lg">
+        <button
+          onClick={() => signOut()}
+          className="flex w-fit px-3 py-1 bg-red-500 text-white rounded-lg"
+        >
           Logout
         </button>
       </div>
