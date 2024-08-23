dc25e5344960a01209c570e2b214ed487abbfefc
Hassan Kaeru <hassanaskary29@gmail.com>
2024-08-24 01:27:16 +0700

    next-auth route

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,56 @@
+import { connectMongoDB } from "@/libs/mongodb";
+import User from "@/models/user";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { User as NextAuthUser } from "next-auth";
+
+interface Credentials {
+  email: string;
+  password: string;
+}
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+
+      async authorize(credentials: Credentials) {
+        const { email, password } = credentials;
+
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            return null;
+          }
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (!passwordMatch) {
+            return null;
+          }
+
+          // Return user object with correct type
+          return user as NextAuthUser;
+        } catch (error) {
+          console.error("Error during authorization:", error);
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET || "",
+  pages: {
+    signIn: "/",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
